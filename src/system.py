38381a121ed1c54429c62ab3f5502cd149127364
XHeavyHitter <viliusprialgauskas@gmail.com>
2026-07-14 21:55:05 +0300

    Velocities for System added

--- src/system.py
+++ src/system.py
@@ -1,12 +1,14 @@
 import numpy as np
 class System:
     def __init__(self, n_cell, rho_star, dt, r_c, T_star):
+        #atributes
         self.n_cell = n_cell
         self.rho_star = rho_star
         self.dt = dt
         self.r_c = r_c
         self.T_star = T_star
         self.N = 4 * n_cell**3
+        #positions
         self.L_star = (self.N / rho_star)**(1/3)
         offsets=[(0, 0, 0), (0.5, 0.5, 0), (0.5, 0, 0.5), (0, 0.5, 0.5)]
         a=self.L_star/self.n_cell
@@ -21,3 +23,7 @@ class System:
                         z = corner[2] + offset[2]*a
                         positions_list.append((x, y, z))
         self.positions = np.array(positions_list)
+        #velocities
+        velocities=np.random.normal(0, np.sqrt(T_star), (self.N, 3))
+        velocities -= np.mean(velocities, axis=0)
+        self.velocities = velocities
